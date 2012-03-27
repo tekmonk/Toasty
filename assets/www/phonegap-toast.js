9d0aacd3cf402ea4064d0f69bc9fd12f4dc2b453
Barmang <gettinstuff@gmail.com>
2012-03-27 15:56:50 -0600

    attempting to update for cordova 1.5

--- assets/www/phonegap-toast.js
+++ assets/www/phonegap-toast.js
@@ -1,17 +1,18 @@
-/*global PhoneGap*/
+// toasty for cordova 1.5 
 var Toasty = function() {
 };
+var exec = cordova.require('cordova/exec');
 
 Toasty.prototype.showLong = function(message, win, fail) {
-  PhoneGap.exec(win, fail, "Toasty", "show_long", [message]);
+ exec(win, fail, "Toasty", "show_long", [message]);
 };
 
 Toasty.prototype.showShort = function(message, win, fail) {
-  PhoneGap.exec(win, fail, "Toasty", "show_short", [message]);
+ exec(win, fail, "Toasty", "show_short", [message]);
 };
 
 Toasty.prototype.cancel = function(win, fail) {
-  PhoneGap.exec(win, fail, "Toasty", "cancel", []);
+ exec(win, fail, "Toasty", "cancel", []);
 };
 
-navigator.toast = new Toasty();
+module.exports = new Toasty();
